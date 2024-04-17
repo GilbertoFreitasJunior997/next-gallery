587bceb7238497e404676b8d840b956f5ec1624b
BetoFreitas <gilberto@frgnet.com.br>
2024-04-17 14:43:17 -0300

    chore: updated dev command

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
+import { type PropsWithChildren } from "react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,11 +14,7 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>{children}</body>
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,3 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
-import { sql } from "drizzle-orm";
 import {
   index,
   pgTableCreator,
@@ -10,12 +6,8 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
+import { sql } from "drizzle-orm";
+
 export const createTable = pgTableCreator((name) => `gallery_${name}`);
 
 export const posts = createTable(
@@ -30,5 +22,5 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
